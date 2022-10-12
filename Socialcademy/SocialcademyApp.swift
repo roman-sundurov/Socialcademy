c886bd51e6cd63d861080f8ecd04f5067e9c4d4f
Roman Sundurov <roman.sundurov.work@gmail.com>
2022-10-12 14:06:49 +0300

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Roman on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
