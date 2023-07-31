7e929852c4afa3cc70fe3e171a1fd862b8b92bd8
Roman Sundurov <roman.sundurov.work@gmail.com>
2023-07-31 05:31:37 +0400

    Fix the preview bug in the AuthView

--- Socialcademy/Views/AuthView.swift
+++ Socialcademy/Views/AuthView.swift
@@ -7,92 +7,121 @@
 
 import SwiftUI
 
+enum AuthForm {
+    case signInForm
+    case createAccount
+}
+
 struct AuthView: View {
     @StateObject var viewModel = AuthViewModel()
+    @State var authForm = AuthForm.signInForm
 
   var body: some View {
     if let user = viewModel.user {
       MainTabView()
         .environmentObject(ViewModelFactory(user: user))
     } else {
-      NavigationView {
-        SignInForm(viewModel: viewModel.makeSignInViewModel()) {
-          NavigationLink("Create Account", destination: CreateAccountForm(viewModel: viewModel.makeCreateAccountViewModel()))
+        NavigationView {
+            switch authForm {
+            case .signInForm:
+                SignInForm(viewModel: viewModel.makeSignInViewModel(), authForm: $authForm)
+            case .createAccount:
+                CreateAccountForm(viewModel: viewModel.makeCreateAccountViewModel(), authForm: $authForm)
+            }
         }
-      }
     }
   }
 }
 
-private extension AuthView {
-  struct Form<Content: View, Footer: View>: View {
-    @ViewBuilder let content: () -> Content
-    @ViewBuilder let footer: () -> Footer
+// private extension AuthView {
+//   struct Form<Content: View, Footer: View>: View {
+//     @ViewBuilder let content: () -> Content
+//     @ViewBuilder let footer: () -> Footer
+// 
+//     var body: some View {
+//       VStack {
+//         Text("Socialcademy")
+//           .font(.title.bold())
+//         content()
+//           .padding()
+//           .background(Color.secondary.opacity(0.15))
+//           .cornerRadius(10)
+//         footer()
+//       }
+//       .navigationBarHidden(true)
+//       .padding()
+//     }
+//   }
+// }
 
-    var body: some View {
-      VStack {
-        Text("Socialcademy")
-          .font(.title.bold())
-        content()
-          .padding()
-          .background(Color.secondary.opacity(0.15))
-          .cornerRadius(10)
-        footer()
-      }
-      .navigationBarHidden(true)
-      .padding()
+private extension AuthView {
+    struct CreateAccountForm: View {
+        @Environment(\.dismiss) private var dismiss
+        @StateObject var viewModel: AuthViewModel.CreateAccountViewModel
+        @Binding var authForm: AuthForm
+
+        var body: some View {
+            VStack {
+              Text("Socialcademy")
+                .font(.title.bold())
+                Group {
+                    TextField("Name", text: $viewModel.name)
+                        .textContentType(.name)
+                    TextField("Email", text: $viewModel.email)
+                        .textContentType(.emailAddress)
+                    SecureField("Password", text: $viewModel.password)
+                        .textContentType(.newPassword)
+                }
+                .padding()
+                .background(Color.secondary.opacity(0.15))
+                .cornerRadius(10)
+
+                Button("Create Account", action: viewModel.submit)
+                    .buttonStyle(.primary)
+                Button("Sign In", action: {
+                    authForm = .signInForm
+
+                })
+            }
+            .padding()
+            .onSubmit(viewModel.submit)
+            .alert("Cannot Create Account", error: $viewModel.error)
+            .disabled(viewModel.isWorking)
+        }
     }
-  }
 }
 
 private extension AuthView {
-  struct CreateAccountForm: View {
-    @StateObject var viewModel: AuthViewModel.CreateAccountViewModel
-    @Environment(\.dismiss) private var dismiss
+    struct SignInForm: View {
+        @StateObject var viewModel: AuthViewModel.SignInViewModel
+        @Binding var authForm: AuthForm
 
-    var body: some View {
-      Form {
-        TextField("Name", text: $viewModel.name)
-          .textContentType(.name)
-        TextField("Email", text: $viewModel.email)
-          .textContentType(.emailAddress)
-        SecureField("Password", text: $viewModel.password)
-          .textContentType(.newPassword)
-      } footer: {
-        Button("Create Account", action: viewModel.submit)
-          .buttonStyle(.primary)
-        Button("Sign In", action: dismiss.callAsFunction)
-          .padding()
-      }
-      .onSubmit(viewModel.submit)
-      .alert("Cannot Create Account", error: $viewModel.error)
-      .disabled(viewModel.isWorking)
+        var body: some View {
+            VStack {
+                Text("Socialcademy")
+                    .font(.title.bold())
+                Group {
+                    TextField("Email", text: $viewModel.email)
+                        .textContentType(.emailAddress)
+                    SecureField("Password", text: $viewModel.password)
+                        .textContentType(.password)
+                }
+                .padding()
+                .background(Color.secondary.opacity(0.15))
+                .cornerRadius(10)
+
+                Button("Sign In", action: viewModel.submit)
+                    .buttonStyle(.primary)
+                Button("Create Account", action: {
+                    authForm = .createAccount
+                })
+            }
+            .padding()
+            .onSubmit(viewModel.submit)
+            .alert("Cannot Sign In", error: $viewModel.error)
+            .disabled(viewModel.isWorking)
+        }
     }
-  }
-}
-
-private extension AuthView {
-  struct SignInForm<Footer: View>: View {
-    @StateObject var viewModel: AuthViewModel.SignInViewModel
-    @ViewBuilder let footer: () -> Footer
-
-    var body: some View {
-      Form {
-        TextField("Email", text: $viewModel.email)
-          .textContentType(.emailAddress)
-        SecureField("Password", text: $viewModel.password)
-          .textContentType(.password)
-      } footer: {
-        Button("Sign In", action: viewModel.submit)
-          .buttonStyle(.primary)
-        footer()
-          .padding()
-      }
-      .onSubmit(viewModel.submit)
-      .alert("Cannot Sign In", error: $viewModel.error)
-      .disabled(viewModel.isWorking)
-    }
-  }
 }
 
 struct AuthView_Previews: PreviewProvider {
